fd7ffda541cbbcd6d1300210f5b661c40dfc1759
KureninovArtem <97453414+KureninovArtem@users.noreply.github.com>
2022-07-27 22:50:23 +0300

    FinishTask-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+import "simplelightbox/dist/simple-lightbox.min.css"
+import SimpleLightbox from "simplelightbox";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const gallery = greateGallery(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", gallery);
+
+function greateGallery(array) {
+    return array.reduce(
+        (acc, item) =>
+            acc + `
+            <a class="gallery__item" href="${item.original}">
+            <img
+            class="gallery__image"
+            src="${item.preview}"
+            alt="${item.description}"
+            />
+            </a>`,
+        "");
+};
+
+new SimpleLightbox(".gallery__item", {
+    captionsData: "alt",
+    captionDelay: 250,
+    captionPosition: "bottom",
+});
